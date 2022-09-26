889ebb063955b9a26e7be3da5aecc01771cb4a7a
Sandro Lenz <sandro.lenz@edu.tbz.ch>
2022-09-26 12:00:22 +0530

    legal docs language options

---
+++ js/language.js
@@ -0,0 +1,25 @@
+var urlParams = new URLSearchParams(window.location.search);
+var urlLanguage = urlParams.get("lang");
+var currentDocument = document.currentScript.getAttribute('doc');
+
+switch (urlLanguage) {
+  case "en":
+    changeLanguage(currentDocument, "en", "de");
+    break;
+  case "de":
+    changeLanguage(currentDocument, "de", "en");
+    break;
+  default:
+    changeLanguage(currentDocument, "en", "de");
+    break;
+}
+
+function changeLanguage(doc, lang, oldLang) {
+  // alert(doc + " " + lang)
+
+  var section = document.getElementById(doc + "-" + lang)
+  section.classList.remove("legal-hidden")
+
+  var oldSection = document.getElementById(doc + "-" + oldLang)
+  oldSection.classList.add("legal-hidden")
+}
